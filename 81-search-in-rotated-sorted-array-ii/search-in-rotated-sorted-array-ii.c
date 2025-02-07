69d7fbb5f5aa5452ced8dd534eca8abcda4f96f9
AlimpanMukherjee <alimpanmukherjee2005@gmail.com>
2025-02-08 02:39:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (21.99%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.c
@@ -0,0 +1,10 @@
+bool search(int* nums, int numsSize, int target) {
+    int i;
+    for(i=0;i<numsSize;i++)
+    {
+        if(nums[i]==target)
+        {
+            return true;
+        }
+    }return false;
+}

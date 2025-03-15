a8bbe1bd62c33f3ac45ce673df3d961822e31586
AlimpanMukherjee <alimpanmukherjee2005@gmail.com>
2025-03-16 04:57:18 +0530

    Time: 3 ms (36.40%) | Memory: 45.4 MB (42.70%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -2,25 +2,23 @@ class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
         int m = dungeon.length;
         int n = dungeon[0].length;
-        int f;
-
         int[][] dp = new int[m][n];
 
-        for(int i = m - 1; i >= 0; i--) {
-             for (int j = n - 1; j >= 0; j--){
-                 int delta = dungeon[i][j];
-                 if(i==m-1 && j==n-1)
-                 {
-                    dp[i][j]=Math.max(1,1-delta);
-                 }
-                 else{
-                    if (j + 1 == n)f=dp[i+1][j];
-                    else if (i + 1 == m)f=dp[i][j+1];
-                    else f=Math.min(dp[i][j+1],dp[i+1][j]);
-                    
-                    dp[i][j]=Math.max(1,f-delta);
-                 }
-             }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int delta = dungeon[i][j];
+
+                if (i == m - 1 && j == n - 1) {
+                    dp[i][j] = Math.max(1, 1 - delta);
+                } else {
+                    int f = Integer.MAX_VALUE;
+
+                    if (j + 1 < n) f = dp[i][j + 1]; // Right cell
+                    if (i + 1 < m) f = Math.min(f, dp[i + 1][j]); // Down cell
+
+                    dp[i][j] = Math.max(1, f - delta);
+                }
+            }
         }
         return dp[0][0];
     }

3b841b17363d3bac9be718e8fa1029fcc06be74a
AlimpanMukherjee <alimpanmukherjee2005@gmail.com>
2025-04-25 20:03:24 +0530

    Time: 2 ms (13.14%) | Memory: 47.9 MB (48.24%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.java
@@ -0,0 +1,18 @@
+
+
+public class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        int hIndex = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (citations[n - 1 - i] >= i + 1) {
+                hIndex = i + 1;
+            } else {
+                break;
+            }
+        }
+        return hIndex;
+    }
+}

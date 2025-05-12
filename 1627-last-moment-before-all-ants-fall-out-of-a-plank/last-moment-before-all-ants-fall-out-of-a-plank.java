c553847ca3e5df9248c40501f0c479208b50fb2d
AlimpanMukherjee <alimpanmukherjee2005@gmail.com>
2025-05-12 13:52:49 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (11.58%) - LeetSync

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.java
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.java
@@ -1,48 +1,59 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public int getLastMoment(int n, int[] left, int[] right) {
+//         List<Integer> leftstck = new ArrayList<>();
+//         List<Integer> rightstck = new ArrayList<>();
+
+//         for (int i = 0; i < left.length; i++) {
+//             leftstck.add(left[i]);
+//         }
+
+//         for (int i = 0; i < right.length; i++) {
+//             rightstck.add(right[i]);
+//         }
+
+//         int t = 0;
+
+//         while (leftstck.size() > 0 || rightstck.size() > 0) {
+//             t++;
+//             for (int i = 0; i < leftstck.size(); ) {
+//                 leftstck.set(i, leftstck.get(i) - 1);
+//                 if (leftstck.get(i) == -1) {
+//                     leftstck.remove(i);
+//                 } else {
+//                     i++;
+//                 }
+//             }
+
+//             // Move right-moving ants
+//             for (int i = 0; i < rightstck.size(); ) {
+//                 rightstck.set(i, rightstck.get(i) + 1);
+//                 if (rightstck.get(i) == n + 1) {
+//                     rightstck.remove(i);
+//                 } else {
+//                     i++;
+//                 }
+//             }
+//         }
+
+//         return t-1;
+//     }
+// }
+
 
 class Solution {
     public int getLastMoment(int n, int[] left, int[] right) {
-        List<Integer> leftstck = new ArrayList<>();
-        List<Integer> rightstck = new ArrayList<>();
+        int maxTime = 0;
 
-        for (int i = 0; i < left.length; i++) {
-            leftstck.add(left[i]);
+        for (int l : left) {
+            maxTime = Math.max(maxTime, l); // time to fall off for left-going ant
         }
 
-        for (int i = 0; i < right.length; i++) {
-            rightstck.add(right[i]);
+        for (int r : right) {
+            maxTime = Math.max(maxTime, n - r); // time to fall off for right-going ant
         }
 
-        int t = 0;
-
-        while (leftstck.size() > 0 || rightstck.size() > 0) {
-            // Move left-moving ants
-            t++;
-            for (int i = 0; i < leftstck.size(); ) {
-                leftstck.set(i, leftstck.get(i) - 1);
-                if (leftstck.get(i) == -1) {
-                    leftstck.remove(i);
-                } else {
-                    i++;
-                }
-            }
-
-            // Move right-moving ants
-            for (int i = 0; i < rightstck.size(); ) {
-                rightstck.set(i, rightstck.get(i) + 1);
-                if (rightstck.get(i) == n + 1) {
-                    rightstck.remove(i);
-                } else {
-                    i++;
-                }
-            }
-
-            //Only increment time if ants are still on the plank
-            // if (leftstck.size() > 0 || rightstck.size() > 0) {
-            //     t++;
-            // }
-        }
-
-        return t-1;
+        return maxTime;
     }
 }

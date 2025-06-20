effd095bd2ddba02e0bfaf72c87d6ecd16966801
AlimpanMukherjee <alimpanmukherjee2005@gmail.com>
2025-06-20 12:59:22 +0530

    Time: 30 ms (95.12%) | Memory: 55.7 MB (48.17%) - LeetSync

--- 2355-maximum-consecutive-floors-without-special-floors/maximum-consecutive-floors-without-special-floors.java
+++ 2355-maximum-consecutive-floors-without-special-floors/maximum-consecutive-floors-without-special-floors.java
@@ -1,24 +1,6 @@
 class Solution {
     public int maxConsecutive(int bottom, int top, int[] special) {
-        // Set <Integer> n=new HashSet<>();
-        // int count=0,maxcount=0;
-        // for(int num:special)
-        // {
-        //     n.add(num);
-        // }
-        // for(int i=bottom;i<=top;i++)
-        // {
-        //     if(n.contains(i))
-        //     {
-        //         count=0;
-        //     }
-        //     else
-        //     {
-        //         count++;
-        //         maxcount=Math.max(maxcount,count);
-        //     }
-        // }
-        int count=0;
+        
         int maxcount=0;
         Arrays.sort(special);
         for(int num:special)

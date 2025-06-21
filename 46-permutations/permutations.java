b41ef6b039ee290726ecceae8517b64b189aed5e
AlimpanMukherjee <alimpanmukherjee2005@gmail.com>
2025-06-22 00:08:40 +0530

    Time: 2 ms (38.72%) | Memory: 44.8 MB (30.52%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int factorial(int n) {
+        int ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans *= i;
+        }
+        return ans;
+    }
+
+    public void reverse(List<Integer> a, int low, int high) {
+        while (low < high) {
+            int temp = a.get(low); // FIX: 'nums' -> 'a'
+            a.set(low, a.get(high));
+            a.set(high, temp);
+            low++;
+            high--;
+        }
+    }
+
+    public List<Integer> generate(List<Integer> a) {  // FIX: return type was '[]int', changed to 'List<Integer>'
+        int n = a.size();  // FIX: was 'nums.size()', but 'nums' not defined yet
+        int dip_index = -1;
+
+        List<Integer> nums = new ArrayList<>(a);  // FIX: moved before size usage
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums.get(i) < nums.get(i + 1)) {
+                dip_index = i;
+                break;
+            }
+        }
+
+        if (dip_index == -1) {
+            Collections.reverse(nums);
+            return nums;  // FIX: added return when reversed
+        }
+
+        for (int i = n - 1; i > dip_index; i--) {
+            if (nums.get(i) > nums.get(dip_index)) {
+                int temp = nums.get(i);
+                nums.set(i, nums.get(dip_index));
+                nums.set(dip_index, temp);
+                break;
+            }
+        }
+
+        reverse(nums, dip_index + 1, n - 1);
+        return nums;  // FIX: return actual list, 'next' was undefined
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> dummy = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            dummy.add(nums[i]);
+        }
+        ans.add(new ArrayList<>(dummy)); // FIX: add copy, not reference
+
+        for (int i = 1; i < factorial(nums.length); i++) {
+            dummy = generate(dummy);
+            ans.add(new ArrayList<>(dummy));  // FIX: add copy again to avoid reference issues
+        }
+
+        return ans;
+    }
+}

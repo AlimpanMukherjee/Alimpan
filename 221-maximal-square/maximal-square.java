c9e0331b3964edd90b1210e9e2e2c97a3176a8b9
Alimpan Mukherjee <alimpanmukherjee2005@gmail.com>
2026-05-30 13:51:35 +0530

    Time: 850 ms (5.05%) | Memory: 75.8 MB (12.61%) - LeetSync

---
+++ 221-maximal-square/maximal-square.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int dp[][]=new int[n][m];
+        int maxi=Integer.MIN_VALUE;
+        for(int i=0;i<m;i++)
+        {
+            if(matrix[0][i]=='0')
+            {
+                dp[0][i]=0;
+            }
+            else
+            {
+                dp[0][i]=1;
+            }
+            maxi=Math.max(maxi,dp[0][i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(matrix[i][0]=='0')
+            {
+                dp[i][0]=0;
+            }
+            else
+            {
+            dp[i][0]=1;
+            }
+            maxi=Math.max(maxi,dp[i][0]);
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                if(matrix[i][j]=='0')dp[i][j]=0;
+                else
+                {
+                    int up=dp[i-1][j];
+                    int left=dp[i][j-1];
+                    int diag=dp[i-1][j-1];
+                    dp[i][j]=1+Math.min(diag,Math.min(left,up));
+                    maxi=Math.max(maxi,dp[i][j]);
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                System.out.println(dp[i][j]);
+            }
+            System.out.println("\n");
+        }
+        
+        return maxi*maxi;
+    }
+}

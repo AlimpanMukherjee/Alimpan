94998086016a268185f5c8c94c523385f0edfd4a
Alimpan Mukherjee <alimpanmukherjee2005@gmail.com>
2026-05-30 22:06:41 +0530

    Time: 42 ms (7.47%) | Memory: 49.9 MB (6.45%) - LeetSync

---
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -0,0 +1,95 @@
+import java.util.*;
+
+class Pair{
+    double m,c,k;
+
+    Pair(double k,double m,double c)
+    {
+        this.m=m;
+        this.c=c;
+        this.k=k;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(this==obj)return true;
+        if(obj==null || getClass()!=obj.getClass())return false;
+
+        Pair p=(Pair)obj;
+
+        return Math.abs(k-p.k)<1e-9 &&
+               Math.abs(m-p.m)<1e-9 &&
+               Math.abs(c-p.c)<1e-9;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        long a=Math.round(k*1e9);
+        long b=Math.round(m*1e9);
+        long d=Math.round(c*1e9);
+
+        return Objects.hash(a,b,d);
+    }
+}
+
+class Solution {
+
+    public int maxPoints(int[][] points) {
+
+        if(points.length==1)return 1;
+        if(points.length==2)return 2;
+
+        int n=points.length;
+
+        Set<Pair> set=new HashSet<>();
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                double dx=points[j][0]-points[i][0];
+                double dy=points[j][1]-points[i][1];
+
+                if(dx==0)
+                {
+                    set.add(new Pair(0,-1,points[i][0]));
+                }
+                else if(dy==0)
+                {
+                    set.add(new Pair(1,0,points[i][1]));
+                }
+                else
+                {
+                    double m=dy/dx;
+                    double c=points[i][1]-m*points[i][0];
+
+                    set.add(new Pair(1,m,c));
+                }
+            }
+        }
+
+        int maxi=2;
+
+        for(Pair p:set)
+        {
+            int cnt=0;
+
+            for(int i=0;i<n;i++)
+            {
+                double lhs=p.k*points[i][1];
+                double rhs=p.m*points[i][0]+p.c;
+
+                if(Math.abs(lhs-rhs)<1e-9)
+                {
+                    cnt++;
+                }
+            }
+
+            maxi=Math.max(maxi,cnt);
+        }
+
+        return maxi;
+    }
+}

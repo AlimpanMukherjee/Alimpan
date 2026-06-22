1b80626de6c96735a7f12bd8f3910ff0a3751e63
Alimpan Mukherjee <alimpanmukherjee2005@gmail.com>
2026-06-22 22:07:49 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (49.96%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,56 @@
 class Solution {
-    public int searchfirst(int[] a, int l, int r, int x) {
-        if (r >= l && l <= a.length - 1) {
-            int mid = l + (r - l) / 2;
-
-            if (mid > 0 && a[mid] == x && a[mid - 1] == x) {
-                return searchfirst(a, l, mid - 1, x);
-            } else if (a[mid] == x) {
-                return mid;
+    int first(int arr[],int target)
+    {
+        int l=0;
+        int h=arr.length-1;
+        int ans=-1;
+        while(l<=h)
+        {
+            int m=(l+h)/2;
+            if(arr[m]<target)
+            {
+                l=m+1;
             }
-
-            if (a[mid] > x) {
-                return searchfirst(a, l, mid - 1, x);
-            } else {
-                return searchfirst(a, mid + 1, r, x);
-            }
-        }
-        return -1;
-    }
-
-        public int searchlast(int[] a, int l, int r, int x) {
-        if (r >= l && l <= a.length - 1) {
-            int mid = l + (r - l) / 2;
-            if (mid < a.length - 1 && a[mid] == x && a[mid + 1] == x) {
-                return searchlast(a, mid + 1, r, x);
-            } else if (a[mid] == x) {
-                return mid;
-            }
-
-            if (a[mid] > x) {
-                return searchlast(a, l, mid - 1, x);
-            } else {
-                return searchlast(a, mid + 1, r, x);
+            else
+            {
+                if(arr[m]==target)ans=m;
+                h=m-1;
             }
         }
-        return -1; 
+        return ans;
     }
-public int[] searchRange(int[] a, int target) {
-        int first = searchfirst(a, 0, a.length - 1, target);
-        int last = searchlast(a, 0, a.length - 1, target);
-        int b[]=new int[2];
-        b[0]=first;
-        b[1]=last;
-        return b;
+
+    int second(int arr[],int target)
+    {
+        int l=0;
+        int h=arr.length-1;
+        int ans=-1;
+        while(l<=h)
+        {
+            int m=(l+h)/2;
+            if(arr[m]<=target)
+            {
+                if(arr[m]==target)ans=m;
+                l=m+1;
+            }
+            else
+            {                
+                h=m-1;
+            }
+        }
+        return ans;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        
+        int ans[]=new int[2];
+        if(nums.length==0)
+        {
+            ans[0]=-1;
+            ans[1]=-1;
+            return ans;
+        }
+        ans[0]=first(nums,target);
+        ans[1]=second(nums,target);
+        return ans;
     }
 }

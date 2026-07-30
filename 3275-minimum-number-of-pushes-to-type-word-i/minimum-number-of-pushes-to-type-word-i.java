b08e146c43f418e7b55cc7d476c8e25ecddef060
Alimpan Mukherjee <alimpanmukherjee2005@gmail.com>
2026-07-31 02:46:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (16.67%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minimumPushes(String word) {
+        //int count=0;
+        if(word.length()<=8)return word.length();
+        else if(word.length()<=16)return word.length()+(word.length()-8);
+        else if(word.length()<=24)return word.length()+(word.length()-8)+(word.length()-16);
+        else return word.length()+(word.length()-8)+(word.length()-16)+(word.length()-24);
+        //return count;
+    }
+}
